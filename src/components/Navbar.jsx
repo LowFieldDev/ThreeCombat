6c5d124ff84364674500df65bc32b8c0583aac4a
Francesco Campobasso <francesco.campobasso@mia-platform.eu>
2023-12-29 15:22:48 +0100

    implemented island rotation (almost warking)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <NavLink to="/" className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md">
             <p>FC</p>
         </NavLink>
-        <nav className="flext text-lg gap-7 font-medium">
+        <nav className="flex text-lg gap-7 font-medium">
             <NavLink to="/about" className={({isActive})=> isActive ? 'text-blue-500' : 'text-black'
         }>
             About
